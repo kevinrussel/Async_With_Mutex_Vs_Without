f49465a33681edf2abfdbf2465dffb69da073bab
Kevin Russel <kzone159@gmail.com>
2026-06-17 16:16:34 -0500

    transforming the data

--- transform_data.py
+++ transform_data.py
@@ -4,5 +4,11 @@ def main():
     with open("url.txt", "r") as file:
         lines = file.readlines()
     
-    print(lines[0])
-    time.sleep(5)
+    print(lines[0].split(","))
+
+    for line in lines:
+        postfix = line.split(",")[1]
+        print(postfix)
+        time.sleep(5)
+
+main()
