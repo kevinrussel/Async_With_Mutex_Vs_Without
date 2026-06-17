9d6c80b654b492e2b5b0529192e141a01fa53849
Kevin Russel <kzone159@gmail.com>
2026-06-17 15:39:24 -0500

    adding code

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import asyncio
 import aiohttp
+import time
 
-
-async def main():
+async def test1():
     pass
-
-asyncio.run(main())
+test_1_time_start = time.time()
+asyncio.run(test1())
+test_1_time_end = time.time()
+print(f"Total time for test 1 is {test_1_time_end - test_1_time_start}")
