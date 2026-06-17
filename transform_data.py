fe270a93c49d217093796877192cc64a01da542e
Kevin Russel <kzone159@gmail.com>
2026-06-17 17:20:15 -0500

    this is working with 150 urls

--- transform_data.py
+++ transform_data.py
@@ -5,11 +5,14 @@ def main():
         lines = file.readlines()
     
     print(lines[0].split(","))
+    counter = 0
     with open("data.txt", "a") as file:
         for line in lines:
+            if ( counter > 300):
+                break
             postfix = line.split(",")[1]
             url = "https://"+postfix
             file.write(url)
-            
+            counter +=1
 
 main()
