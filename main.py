5474046483e1203925c333fe093305bebb55007a
Kevin Russel <kzone159@gmail.com>
2026-06-17 16:25:30 -0500

    adding code

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ import asyncio
 import aiohttp
 import time
 
-async def test1():
-    pass
+async def test1(session,url):
+    async with session.get(url) as response:
+        status = response.status
+        return status
 
 
 
 async def test1():
     async with aiohttp.ClientSession() as session:
         responses = []
-
-
         async with asyncio.TaskGroup() as tg:
             pass
 
@@ -20,7 +20,7 @@ async def test1():
 
 
 
-test_1_time_start = time.time()
+test_1_time_start = time.perf_counter()
 asyncio.run(test1())
-test_1_time_end = time.time()
+test_1_time_end = time.perf_counter()
 print(f"Total time for test 1 is {test_1_time_end - test_1_time_start}")
