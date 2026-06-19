a1f9e762c795adea9dc0624302df5ee182c5b480
Kevin Russel <kzone159@gmail.com>
2026-06-18 22:08:48 -0500

    adding code

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import aiohttp
 import time
 
 
+
 async def test1_url(session,url, semaphore):
     async with semaphore:
         try:
@@ -55,3 +56,12 @@ test_1_time_start = time.perf_counter()
 asyncio.run(test1())
 test_1_time_end = time.perf_counter()
 print(f"Total time for test 1 is {test_1_time_end - test_1_time_start}")
+
+
+
+
+async def test2():
+
+
+
+time_2_time_start = time.perf_counter()
