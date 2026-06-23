52e2010dd1d01237dfb012b18b08fe044f1e3a84
Kevin Russel <kzone159@gmail.com>
2026-06-22 22:28:58 -0500

    commit before long run

--- main.py
+++ main.py
@@ -211,7 +211,8 @@ def main():
     data_file = "url/data.txt"
     num_of_packets = [1,2,10,50,100,250,500,1000,2500,5000,7000,10000]
     run_test(num_of_packets,data_file,async_file_path,async_runs_result_path)
+    run_test(num_of_packets,data_file,threading_file_path,threading_runs_results_path)
     run_test(num_of_packets,data_file,sequential_file_path,sequential_runs_result_path)
-    run_test(num_of_packets,data_file,threading_file_path,threading_runs_results_path)     
+         
 main()
 
